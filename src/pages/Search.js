7c7c56c783e4a06aff2ce0def4232d2b66e53fa4
alt-art <pedromendescraft@gmail.com>
2021-11-13 16:24:31 -0300

    feat(search): List artist album

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,8 +9,12 @@ class Search extends React.Component {
     this.state = {
       input: '',
       disabled: true,
+      loading: false,
+      message: '',
+      artists: [],
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleInputChange(event) {
@@ -20,25 +26,68 @@ class Search extends React.Component {
     });
   }
 
+  handleClick() {
+    const { input } = this.state;
+    this.setState({
+      input: '',
+      disabled: true,
+      loading: true,
+    });
+    searchAlbumsAPI(input).then((data) => {
+      if (data.length === 0) {
+        this.setState({
+          message: 'Nenhum álbum foi encontrado',
+        });
+      } else {
+        this.setState({
+          message: `Resultado de álbuns de: ${input}`,
+          artists: data,
+        });
+      }
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { input, disabled } = this.state;
-    return (
+    const { input, disabled, loading, message, artists } = this.state;
+    return !loading ? (
       <div data-testid="page-search">
         <Header />
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          placeholder="Search"
-          value={ input }
-          onChange={ this.handleInputChange }
-        />
-        <input
-          type="button"
-          value="Search"
-          data-testid="search-artist-button"
-          disabled={ disabled }
-        />
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Search"
+            value={ input }
+            onChange={ this.handleInputChange }
+          />
+          <input
+            type="button"
+            value="Search"
+            data-testid="search-artist-button"
+            onClick={ this.handleClick }
+            disabled={ disabled }
+          />
+        </div>
+        <div>
+          <p>{message}</p>
+          {artists.map((artist) => (
+            <p key={ artist.collectionId }>
+              <Link
+                key={ artist.collectionId }
+                to={ `/album/${artist.collectionId}` }
+                data-testid={ `link-to-album-${artist.collectionId}` }
+              >
+                { artist.collectionName }
+              </Link>
+            </p>
+          ))}
+        </div>
       </div>
+    ) : (
+      <p>Loading...</p>
     );
   }
 }
